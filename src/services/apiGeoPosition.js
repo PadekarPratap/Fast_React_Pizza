a91ae401eb10017a4f33799a467c4bfad976968f
Pratap Padekar <unitedpratap7@gmail.com>
2023-08-19 14:42:11 +0530

    completed the fast-react-pizza project

--- src/services/apiGeoPosition.js
+++ src/services/apiGeoPosition.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+// `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
 
 export const fetchAddress = async (latitude, longitude) => {
   const { data } = await axios.get(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
+    `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=b2fbbb1ee17b4bd59f44cff1a6a7348b`,
   );
 
-    console.log("addressData", data)
+  console.log('addressData', data);
 
-    const address = `${data?.locality} ${data?.city} ${data?.countryName}`
+  const address = data.results[0].formatted;
 
-    return address
+  return address;
 };
